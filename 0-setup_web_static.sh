3bfd2f9a5109b202333acef40516518176e79a56
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-07 23:53:56 +0100

    changed insert line of sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,6 +19,6 @@ echo '<html>
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo sed -i "17 i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "53 i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 sudo nginx -s reload
